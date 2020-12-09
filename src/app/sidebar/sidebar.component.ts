fdd89425c68683cb109ecf1eea953d6a496b6fbb
usef <usefrady@gmail.com>
2020-12-09 11:46:24 +0200

    Sidebar works

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DocsService } from './docs.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  docs: {id: number, name: string, details: {}}[] = [];
+
+  constructor(private docsService: DocsService) { }
 
   ngOnInit(): void {
+    this.docs = this.docsService.getDocs();
+  }
+
+  addDoc(doc: {id: number, name: string, details: {}}){
+    this.docsService.addNewDoc(doc);
+  }
+
+  deleteDoc(id: number){
+    this.docsService.deleteDoc(id);
+  }
+
+  editDoc(id: number){
+    this.docsService.editDoc(id);
+  }
+  
+  showDoc(event: any){
+    let id = event.target.id;
+    let doc = this.docsService.getDoc(id);
+    //Show doc on page
   }
 
 }
