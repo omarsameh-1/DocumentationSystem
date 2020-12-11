44d91da454df5a94c20d99f204a085dcf8f66a59
ahmedAshref <58373471+ahmedAshrefsw@users.noreply.github.com>
2020-12-11 14:42:10 +0200

    initial-component-without-validate

--- src/app/phase/initial-phase/initial-phase.component.ts
+++ src/app/phase/initial-phase/initial-phase.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DocsService } from 'src/app/sidebar/docs.service';
+
 
 @Component({
   selector: 'app-initial-phase',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InitialPhaseComponent implements OnInit {
 
-  constructor() { }
+  constructor(private docsService:DocsService) { }
 
   ngOnInit(): void {
+  }
+
+  getDoc(){
+    let doc = {};
+    console.log(form);
+    doc.id= this.docsService.getNumberOfDocs()+1;
+    doc.name= 'Project Charter';
+    doc.details = {
+
+      projectTitle : document.getElementById('projectTitle').value,
+      startDate : document.getElementById('startDate').value,
+      endDate : document.getElementById('endDate').value,
+      projectObjectives : document.getElementById('projectObjectives').value,
+      projectManager : document.getElementById('projectManager').value,
+      budgetInformation : document.getElementById('budgetInformation').value,
+      projectScope : document.getElementById('projectScope').value,
+    };
+   this.addDoc(doc);
+  }
+
+  addDoc(doc:any){
+    this.docsService.addNewDoc(doc);
   }
 
 }
