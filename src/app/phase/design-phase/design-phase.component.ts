86d768245761756faed9f848562346f41ce259b1
AmrAhmedRamadan <aamr91437@gmail.com>
2020-12-13 22:15:44 +0200

    DPhase-3

--- src/app/phase/design-phase/design-phase.component.ts
+++ src/app/phase/design-phase/design-phase.component.ts
@@ -10,10 +10,13 @@ export class DesignPhaseComponent implements OnInit {
 
   public files_: any[] = [{
     id: 1,
-    fileName: '',
-    Path:''
+    name: '',
+    type:'image',
+    details:{
+      path:''
+    }
   }];
-  constructor() {
+  constructor(private docsService: DocsService) {
 
   }
 
@@ -21,30 +24,34 @@ export class DesignPhaseComponent implements OnInit {
 
   }
 
-  addAddress() {
+  AddDocument() {
     this.files_.push({
-      id: this.files_.length + 1,
-      fileName: '',
-      Path:''
+      id: this.docsService.getNumberOfDocs() + 1,
+      name: '',
+      type:'image',
+      details:{
+        path:''
+      }
       
     });
   }
 
-  removeAddress(i: number) {
+  RemoveDocument(i: number) {
     this.files_.splice(i, 1);
   }
   handleUpload(event:any){
-    for(let i=0;i<this.addAddress.length+1;i++){
-      this.files_[i].Path = event.target.value;
-      this.files_[i].fileName = event.target.files[i].name;
+    for(let i=0;i<this.files_.length+1;i++){
+      this.files_[i].details.path = event.target.value;
+      this.files_[i].name = event.target.files[i].name;
     }
  }
 
  
 
-  logValue() {
-    console.log(this.files_);
-    
+  Save() {
+    this.files_.forEach((file)=>{
+      this.docsService.addNewDoc(file);
+    });
   }
 
 }
