49bdc9cd538cc0b6c8bb6ffaf27d16e6aa059b2d
usef <usefrady@gmail.com>
2020-12-16 02:20:24 +0200

    fix design component errors

--- src/app/phase/design-phase/design-phase.component.ts
+++ src/app/phase/design-phase/design-phase.component.ts
@@ -10,74 +10,44 @@ import { DocsService } from 'src/app/sidebar/docs.service';
 })
 export class DesignPhaseComponent implements OnInit {
 
-  public files_: Doc[] = new Array<Doc>();
-  doc = <Doc>{
-    id : 0 ,
-    name : '',
-    type:'name',
-    details :{
-      path: ''
-    }
-  }
+  files_: string[] = [];
+  toAdd:Doc[] = [];
+
   constructor(private docsService: DocsService) {
 
   }
 
   ngOnInit() {
-    //this.files_ = this.docsService.getDocs();
   }
 
   AddDocument() {
     // doc means image as a doc for design 
-    // let doc = <Doc>{
-    //   id: 0,
-    //   name: '',
-    //   type:'image',
-    //   details:{
-    //     path:''
-    //   }
-    // }
-    this.files_.push(this.doc);
+    this.files_.push('');
   }
 
   RemoveDocument(i: number) {
     this.files_.splice(i, 1);
   }
   handleUpload(event:any){
-    // for(let i=0;i<this.files_.length+1;i++){
-    //   this.files_[i].details = {
-    //     path: event.target.value
-    //   };
-    //   this.files_[i].name = event.target.files[i].name;
-    // }
-    // let doc = <Doc>{
-    //   id: 0,
-    //   name: '',
-    //   type:'image',
-    //   details:{
-    //   path: event.target.value
-    //   }
-    // }
-    this.doc.details.path = event.target.value; 
-    this.files_.push(doc);
-  //   console.log(event.target.value)
-  //   this.files_.forEach(file => {
-  //     file.details.path = event.target.value ;
-  //  //   file.name = event.target.file.name ;
-  //   });
+    let doc = <Doc>{
+      id: 0,
+      name: event.target.files[0].name,
+      type:'image',
+      details:{
+      path: event.target.value
+      }
+    }
+    this.toAdd.push(doc);
  }
 
  
 
   Save() {
-    
-    console.log(this.files_);
-    this.files_.forEach((file)=>{
-      //console.log(file);
-      this.docsService.addNewDoc(file);
-      
+    this.toAdd.forEach((file)=>{
+      this.docsService.addNewDoc(file);      
     });
     this.files_ = [];
+    this.toAdd = [];
   }
 
 }
