55f6c5c705e9a7c12c181e300061928ebba0a3b6
ahmedAshref <58373471+ahmedAshrefsw@users.noreply.github.com>
2020-12-16 02:03:14 +0200

    trying to solve design func - passed to yousef

--- src/app/phase/design-phase/design-phase.component.ts
+++ src/app/phase/design-phase/design-phase.component.ts
@@ -1,3 +1,4 @@
+import { collectExternalReferences } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Doc } from 'src/app/model/doc.model';
 import { DocsService } from 'src/app/sidebar/docs.service';
@@ -10,45 +11,73 @@ import { DocsService } from 'src/app/sidebar/docs.service';
 export class DesignPhaseComponent implements OnInit {
 
   public files_: Doc[] = new Array<Doc>();
+  doc = <Doc>{
+    id : 0 ,
+    name : '',
+    type:'name',
+    details :{
+      path: ''
+    }
+  }
   constructor(private docsService: DocsService) {
 
   }
 
   ngOnInit() {
-    this.files_ = this.docsService.getDocs();
+    //this.files_ = this.docsService.getDocs();
   }
 
   AddDocument() {
-    let doc = <Doc>{
-      id: 0,
-      name: '',
-      type:'image',
-      details:{
-        path:''
-      }
-    }
-    this.files_.push(doc);
+    // doc means image as a doc for design 
+    // let doc = <Doc>{
+    //   id: 0,
+    //   name: '',
+    //   type:'image',
+    //   details:{
+    //     path:''
+    //   }
+    // }
+    this.files_.push(this.doc);
   }
 
   RemoveDocument(i: number) {
     this.files_.splice(i, 1);
   }
   handleUpload(event:any){
-    for(let i=0;i<this.files_.length+1;i++){
-      this.files_[i].details = {
-        path: event.target.value
-      };
-      this.files_[i].name = event.target.files[i].name;
-    }
+    // for(let i=0;i<this.files_.length+1;i++){
+    //   this.files_[i].details = {
+    //     path: event.target.value
+    //   };
+    //   this.files_[i].name = event.target.files[i].name;
+    // }
+    // let doc = <Doc>{
+    //   id: 0,
+    //   name: '',
+    //   type:'image',
+    //   details:{
+    //   path: event.target.value
+    //   }
+    // }
+    this.doc.details.path = event.target.value; 
+    this.files_.push(doc);
+  //   console.log(event.target.value)
+  //   this.files_.forEach(file => {
+  //     file.details.path = event.target.value ;
+  //  //   file.name = event.target.file.name ;
+  //   });
  }
 
  
 
   Save() {
+    
+    console.log(this.files_);
     this.files_.forEach((file)=>{
-      console.log(file);
+      //console.log(file);
       this.docsService.addNewDoc(file);
+      
     });
+    this.files_ = [];
   }
 
 }
